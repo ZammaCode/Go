ccde8b7ddb9faf7b3e3f81fa43fd36bf65676b77
asghar <asghar1075.dev@gmail.com>
2022-03-08 13:06:17 +0500

    02-03 NumericVariables

---
+++ src/02-04 StringVariables/str_variables.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	//declare hw variable statically using
+	//data type string and also assign a value
+	var hw string = "Hello World"
+
+	fmt.Println(hw)
+}
