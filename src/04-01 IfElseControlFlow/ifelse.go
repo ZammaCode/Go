cf579d7632cbc6ba7dba7e80f9b0e4f4813a7d3e
asghar <asghar1075.dev@gmail.com>
2022-03-22 21:55:47 +0500

    04-01 IfElseControlFlow

--- src/04-01 IfElseControlFlow/ifelse.go
+++ src/04-01 IfElseControlFlow/ifelse.go
@@ -19,4 +19,12 @@ func main() {
 		//true then this block will be executed
 		fmt.Println("Both of the above conditions are false")
 	}
+
+	//the composite if allows for initialization
+	//with in if statement e.g. here variable j is
+	//declared and initialized before test condition
+	if j := 2; j == 2 {
+		//this block will be executed if j equal to 2
+		fmt.Println("j == 2 is true")
+	}
 }
